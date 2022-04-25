8b1451353fb3d17b45147ebb780e00f46dcf440e
hassansaber <hassansaberkarin@gmail.com>
2022-04-25 19:51:11 +0430

    done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-<<<<<<< HEAD
 import logo from "./logo.svg";
 import "./App.css";
-=======
-import Logo from "./Logo";
-import CaughtPokemon from "./CaughtPokemon";
-import BestPokemon from "./BestPokemon";
->>>>>>> e74bd509a65cd35f3edaf51901f2afaf2ed3e13c
 
-const App = () => {
+function App() {
   return (
-<<<<<<< HEAD
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -26,14 +18,8 @@ const App = () => {
           Learn React
         </a>
       </header>
-=======
-    <div style={{ textAlign: "center" }}>
-      <Logo />
-      <BestPokemon />
-      <CaughtPokemon />
->>>>>>> e74bd509a65cd35f3edaf51901f2afaf2ed3e13c
     </div>
   );
-};
+}
 
 export default App;
